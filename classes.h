f3b1736a39f1f82cb9ac9e4b2c0a171a014ed901
davanderlinden <46512605+davanderlinden@users.noreply.github.com>
2019-04-18 18:20:59 -0500

    Create classes.h

---
+++ classes.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void progress(int);
+
+class story {
+private:
+	string comment = "this is the begining of the game";
+	string second = "second call";
+public:
+	string getComment();
+	string getSecond();
+};
+
+class fights {
+private:
+
+public:
+};
+
+class traps_obs {
+private:
+
+public:
+};
+
+class stats {
+private:
+
+public:
+};
