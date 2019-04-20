af8025e6b6efd6f31fda216c24206c163fe27f45
krazyforce <40051025+krazyforce@users.noreply.github.com>
2019-04-20 03:04:43 -0500

    Update Outline.cpp

--- Outline.cpp
+++ Outline.cpp
@@ -4,4 +4,35 @@
 
 //Classes Section , use this to put whatever classes and what are in the classes
 
+Display Menu class(); //if else statement , need a loop
+   Start
+      Call on classes for Forest and Dungeon class
+   Exit
+      User decides to quit program
+Forest class ();
+    Story line switch case
+    Inheritance trap class
+Dungeon class ();
+    Story line switch case
+    Inheritance trap class
+Traps OBS Class ();
+    Damage (randomize damage from 1 to 5)
+Stats class ();
+    Health (100/100)
+//Things to add if we have time
+    MP
+    Luck
+
+Items class ();
+    HP pots
+//Things to add if we have time
+    Mp pots
+    Items
+    Weapons
+    Money
+    
+      
+
+
+
 
